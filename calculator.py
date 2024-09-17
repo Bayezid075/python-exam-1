245364cf6b82a286a22055cb96368695c34844cc
Bayezid075 <mrbayezidhoshen@gmail.com>
2024-09-17 20:47:44 +0600

    calculator input validator

--- calculator.py
+++ calculator.py
@@ -7,14 +7,26 @@ while True:
 5 for Modulus (%)
 --> Select your Mode : 
 ''')
-    user_operation_mode = int(user_operation_mode)
-    if user_operation_mode <= 5  and user_operation_mode  != 0 :
-        break
-    else:
-        print("Wrong Operation Mode | Please Choose from above")
+    try: #TypeError Handler
+        user_operation_mode = int(user_operation_mode)
+        if user_operation_mode <= 5  and user_operation_mode  != 0 :
+            break
+        else:
+             print("Wrong Operation Mode | Please Choose from above")
+    except ValueError:
+                print("Enter a Valid Number!!")
 
-user_first_number = float(input("Inter Your First Number : "))
-user_second_number = float(input("Inter Your second Number : "))
+user_first_number = 0
+user_second_number = 0
+while True:
+    try: #Input TypeError Handler
+        user_first_input = float(input("Inter Your First Number : "))
+        user_second_input = float(input("Inter Your second Number : "))
+        user_first_number = user_first_input
+        user_second_number = user_second_input
+        break
+    except ValueError :
+        print("Enter Valid Number!!!")
 
 def addition(a = user_first_number, b = user_second_number):
     print(f"{a} + {b} = {a+b}")
@@ -28,7 +40,7 @@ def division(a = user_first_number, b = user_second_number):
     else:
         print(f"{a} / {b} = {a/b}")
 def modulus(a = user_first_number, b = user_second_number):
-    print(f"{a} / {b} = {a/b}")
+    print(f"{a} % {b} = {a%b}")
 
 if user_operation_mode == 1:
     addition()
@@ -40,6 +52,3 @@ elif user_operation_mode == 4:
     division()
 elif user_operation_mode == 5:
     modulus()
-
-
-
